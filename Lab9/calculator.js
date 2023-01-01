2c04322b249ebfa4a1f95205efac691a2737c159
Parameow <87403363+Parameow3@users.noreply.github.com>
2023-01-01 22:48:52 +0700

    number clickable

---
+++ Lab9/calculator.js
@@ -0,0 +1,90 @@
+
+function numOne(){
+    let numDisplay = document.getElementById("display").value;
+    if(Number(numDisplay) == 0){
+        document.getElementById("display").value = 1;
+    }
+    else
+        document.getElementById("display").value += 1;
+}
+
+function numTwo(){
+    let numDisplay = document.getElementById("display").value;
+    if(Number(numDisplay) == 0){
+        document.getElementById("display").value = 2;
+    }
+    else
+        document.getElementById("display").value += 2;
+}
+
+function numThree(){
+    let numDisplay = document.getElementById("display").value;
+    if(Number(numDisplay) == 0){
+        document.getElementById("display").value = 3;
+    }
+    else
+        document.getElementById("display").value += 3;
+}
+
+function numFour(){
+    let numDisplay = document.getElementById("display").value;
+    if(Number(numDisplay) == 0){
+        document.getElementById("display").value = 4;
+    }
+    else
+        document.getElementById("display").value += 4;
+}
+
+function numFive(){
+    let numDisplay = document.getElementById("display").value;
+    if(Number(numDisplay) == 0){
+        document.getElementById("display").value = 5;
+    }
+    else
+        document.getElementById("display").value += 5;
+}
+
+function numSix(){
+    let numDisplay = document.getElementById("display").value;
+    if(Number(numDisplay) == 0){
+        document.getElementById("display").value = 6;
+    }
+    else
+        document.getElementById("display").value += 6;
+}
+
+function numSeven(){
+    let numDisplay = document.getElementById("display").value;
+    if(Number(numDisplay) == 0){
+        document.getElementById("display").value = 7;
+    }
+    else
+        document.getElementById("display").value += 7;
+}
+
+function numEight(){
+    let numDisplay = document.getElementById("display").value;
+    if(Number(numDisplay) == 0){
+        document.getElementById("display").value = 8;
+    }
+    else
+        document.getElementById("display").value += 8;
+}
+
+function numNine(){
+    let numDisplay = document.getElementById("display").value;
+    if(Number(numDisplay) == 0){
+        document.getElementById("display").value = 9;
+    }
+    else
+        document.getElementById("display").value += 9;
+}
+
+function numZero(){
+    let numDisplay = document.getElementById("display").value;
+    if(Number(numDisplay) == 0){
+        document.getElementById("display").value = 0;
+    }
+    else
+        document.getElementById("display").value += 0;
+}
